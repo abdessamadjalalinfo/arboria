eca31bc29f43568a23378f8008cb88c5e5cc0628
abdessamadjalalinfo <53655103+abdessamadjalalinfo@users.noreply.github.com>
2023-03-01 18:06:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4E5EQ45Z0N"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4E5EQ45Z0N');
+</script>
+
 
 <head>
   <meta charset="utf-8">
