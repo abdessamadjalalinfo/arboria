3baa6f3445cf727a75dd6519be67aa855dbf49d8
abdessamadjalalinfo <53655103+abdessamadjalalinfo@users.noreply.github.com>
2023-03-01 16:26:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,401 @@
-<?php include_once("index.html"); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Demander un devis</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Arsha - v4.11.0
+  * Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html"><img src="assets/img/header-logo.png" alt="" srcset=""></a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Accueil</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Devis</a></li>
+          <li><a class="nav-link scrollto" href="#about">Nos services</a></li>
+
+          <li><a class="getstarted scrollto" href="#contact">Demander un devis</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+          <h1>Choisissez une ASSURANCE adaptée à votre budget</h1>
+          <h2>ÉCONOMISEZ jusqu’à 45%
+            sur votre assurance santé !</h2>
+          <div class="d-flex justify-content-center justify-content-lg-start">
+            <a href="#contact" class="btn-get-started scrollto">Demander un devis</a>
+          </div>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+          <img src="assets/img/doctors.png" class="img-fluid animated" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Clients Section ======= -->
+    <section id="clients" class="clients section-bg">
+      <div class="container">
+
+        <div class="row" data-aos="zoom-in">
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/april-premium.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/Coverity.png" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/Logo_ssfr.svg.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/2MA-MA-MEILLEURE-ASSURANCE-d.png" class="img-fluid" alt="">
+          </div>
+
+        
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/logo_SPVIE_blanccartouchenoir_0.png" class="img-fluid" alt="">
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+            <img src="assets/img/WAZARI.jpg" class="img-fluid" alt="">
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Cliens Section -->
+    
+    <!-- ======= Contact Section ======= -->
+    <section id="contact"  class="contact">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Demander un devis gratuitement</h2>
+          <p>
+            Le devis mutuelle peut être demandé auprès des agences ou société de mutuelles physiques ou en ligne.Pour l’obtenir, le futur adhérent doit juste remplir avec les informations nécessaires la formulaire préétabli fournie par la mutuelle.Le devis complémentaire santé est délivré gratuitement et n’engage le client en rien.
+                         
+          </p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-5 d-flex align-items-stretch">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Adresse:</h4>
+                <p>5 Rue d’Estienne d’Orves, 94000 Créteil, France
+
+                </p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>contact@conseilassur.info</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>Téléphone:</h4>
+                <p>01 02 56 03 03</p>
+              </div>
+
+              <iframe src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Cr%C3%A9teil+(Rue%20d%E2%80%99Estienne%20d%E2%80%99Orves)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+            </div>
+
+          </div>
+
+          <div  class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+            <form   action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div  class="row">
+                
+                <div class="form-group col-md-6">
+                  <label for="moi">Personnes à assurer</label>
+                  <select name="moi" class="form-select">
+                    <option value="Moi">Moi</option>
+                    <option value="Mon couple">Mon couple</option>
+                    <option value="Mon couple et mes enfants">Mon couple et mes enfants</option>
+                    <option value="Mes enfants et moi">Mes enfants et moi</option>
+                   
+  
+  
+                  </select>
+            
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="name">Nom et Prénom</label>
+                  <input type="text" name="namee" class="form-control" id="name" required>
+                </div>
+                
+                <div class="form-group col-md-6">
+                  <label for="name">Email</label>
+                  <input type="email" class="form-control" name="email" id="email" required>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="name">Télephone</label>
+                  <input type="text" class="form-control" placeholder="+331234567890" name="phone" id="phone" required>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="name">Date de Naissance</label>
+                  <input type="date" class="form-control" name="datee" id="date" required>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="name">Code Postal</label>
+                  <input type="text" class="form-control" placeholder="Code postal" name="zip" id="adresse" required>
+                </div>
+              </div>
+              <div class="form-group col-md-6">
+                <label for="name">Mon régime</label>
+                <select name="regime" class="form-select">
+                  <option value="Régime général">Régime général</option>
+                  <option value="Travailleur non salarié">Travailleur non salarié</option>
+                  <option value="xploitant agricole">Exploitant agricole</option>
+                  <option value="Salarié agricole">Salarié agricole</option>
+                  <option value="Alsace Moselle">Alsace Moselle</option>
+                  <option value="Autres">Autres</option>
+
+
+                </select>
+          
+              </div>
+             
+              
+              
+              
+              <div class="text-center"><button type="submit">Demander le devis gratuitement</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+    <!-- ======= About Us Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>A propos de nos services</h2>
+        </div>
+
+        <div class="row content">
+          <div class="col-lg-6">
+            
+            <ul>
+              <li><i class="ri-check-double-line"></i> Elly Assurance, société par actions simplifiée est active depuis plus de 6 ans. Implantée à Creteil (94000), elle est spécialisée dans le secteur des activités des agents et courtiers d’assurances.
+              </li>
+              <li><i class="ri-check-double-line"></i> Elly Assurance est responsable de traiter vos données personnelles, notamment les données personnelles collectées via notre site Internet. Ceci dit, Elly Assurance est responsable de la façon dont vos données sont traitées.
+              </li>
+              <li><i class="ri-check-double-line"></i> La plupart des données personnelles traitées proviennent directement des données saisies par les utilisateurs de Elly Assurance pendant leur demande de devis. Les données collectées sont nécessaires pour obtenir des informations de comparaison et obtenir des devis. </li>
+            </ul>
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0">
+            <p>
+              Chez Elly Assurance nous nous engageons à protéger vos données personnelles, à les traiter avec respect et à considérer leur protection lors de la conception de nos produits et services. Il s’agit d’une politique de confidentialité conçue pour vous aider à comprendre pleinement comment nous collectons, utilisons et protégeons vos données personnelles.
+
+              En communiquant vos données personnelles à Elly Assurance, vous acceptez leur utilisation comme décrit ci-dessous. Quand vous nous fournissez des données personnelles d’un tiers, vous confirmez avoir préalablement obtenu leur consentement.
+
+            </p>
+            
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Us Section -->
+
+    <!-- ======= Why Us Section ======= -->
+    <section id="why-us" class="why-us section-bg">
+      <div class="container-fluid" data-aos="fade-up">
+
+        <div class="row">
+
+          <div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch  order-2 order-lg-1">
+
+            <div class="content">
+              <h3><strong>A quoi sert une assurance santé ?
+              </strong>l</strong></h3>
+              <p>
+                La mutuelle santé vous amène plus de sérénité au quotidien, vous bénéficiez de meilleures garanties vous couvrant pour les soins dits moins classiques, comme : L'optique, dentaire, les soins généraux, l'hospitalisation....
+
+                Afin de réduire vos dépenses de santé, et surtout pour compléter les remboursements de l'Assurance maladie, il est possible de souscrire une mutuelle ou une assurance santé privée. Ces complémentaires santé proposent des formules adaptées à vos besoins en fonction de votre budget surtout. Arboria Santé rembourse partiellement et parfois totalement les dépenses de santé qui restent à votre charge. Obtenez votre devis en 2 minutes !   </p>
+            </div>
+
+            <div class="accordion-list">
+              <ul>
+                <li>
+                  <a data-bs-toggle="collapse" class="collapse" data-bs-target="#accordion-list-1"><span>01</span>Comment trouver une mutuelle sénior pas chère ? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                  <div id="accordion-list-1" class="collapse show" data-bs-parent=".accordion-list">
+                    <p>
+                      Pour chaque offre reçue, vous devrez évaluer le ratio entre les remboursements auxquels vous aurez droit dans l’année pour vos dépenses de santé (lunettes, frais dentaires…) et le coût annuel de la couverture. Si votre mutuelle vous coûte par exemple 600 € par an mais qu’elle vous confère 1 000 € de remboursements dans l’année, le ratio est positif.
+
+                    </p>
+                  </div>
+                </li>
+
+                <li>
+                  <a data-bs-toggle="collapse" data-bs-target="#accordion-list-2" class="collapsed"><span>02</span> Comment trouver une mutuelle étudiant pas chère ?<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                  <div id="accordion-list-2" class="collapse" data-bs-parent=".accordion-list">
+                    <p>
+                      En fonction de leur âge, les étudiants tendent souvent à en demander davantage s’agissant de complémentarité santé, surtout s’ils n’ont aucun besoin en optique ou en dentaire. Une mutuelle santé serait la plus abordable sur ce genre de profil.
+
+                      Bon nombre d’assureurs et d’organismes ont développé une gamme de mutuelles pas chères pour les jeunes et étudiants.. Axa propose par exemple des couvertures d’entrée de gamme à partir de quelques euros par mois, idem pour SMENO (à partir de 12 € / mois pour la première formule) ou la LMDE (à partir de 14 € / mois). Pour ce prix-là, le niveau de couverture sera bien entendu minimum.
+                                        </p>
+                  </div>
+                </li>
+
+                <li>
+                  <a data-bs-toggle="collapse" data-bs-target="#accordion-list-3" class="collapsed"><span>03</span> Comment trouver une mutuelle famille pas chère ?<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                  <div id="accordion-list-3" class="collapse" data-bs-parent=".accordion-list">
+                    <p>
+                      Plusieurs établissements proposent des mutuelles moins coûteuses pour les familles, en appliquant des remises sur les cotisations pour les enfants. Par exemple, Groupama prévoit 5 % de réduction pour le 2e assuré, 20 % pour le 2e enfant et la gratuité de la cotisation à partir du 3e enfant. Cela est donc très intéressant, notamment pour les familles nombreuses.
+
+                      Dans cette optique, il faudra encore comparer de nombreux devis, en adaptant le niveau de garanties souhaité aux besoins réels de la famille, notamment en optique et en dentaire. Via notre comparateur, n’hésitez pas à moduler à la hausse comme à la baisse le niveau de couverture afin de réaliser plusieurs simulations.
+
+                    </p>
+                  </div>
+                </li>
+
+              </ul>
+            </div>
+
+          </div>
+
+          <div class="col-lg-5 align-items-stretch order-1 order-lg-2 img" style='background-image: url("assets/img/phone-credit.png");' data-aos="zoom-in" data-aos-delay="150">&nbsp;</div>
+        </div>
+
+      </div>
+    </section><!-- End Why Us Section -->
+
+
+
+
+
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-newsletter">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-6">
+            <h4>Rejoignez notre newsletter</h4>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="S'inscrire">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>Arsha</h3>
+            <p>
+              8 Rue d’Estienne d’Orves,<br>
+              94000 Créteil,<br>
+              France <br><br>
+              <strong>Télephonehone:</strong> +1 5589 55488 55<br>
+              <strong>Email:</strong> info@example.com<br>
+            </p>
+          </div>
+
+         
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        &copy; Copyright 2023<strong><span>Elly Assurance</span></strong>. Tous droits réservés
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ -->
+        Toute reproduction est strictement interdite</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <!--script src="assets/vendor/php-email-form/validate.js"></script-->
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
